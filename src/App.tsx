7fba7e5eed35de0b5c0d3b73e2ccabd9a9e0b680
inga-divra <ingady@gmail.com>
2024-09-17 12:29:22 +0300

    Finished the challenge by implementing a conditional ProfileCard component that adjusts rendering based on the type prop (basic or advanced), showing email only for the advanced type.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-import Component from './starter/04-events';
+//import Component from './final/05-challenge';
+
+import Component from './starter/05-challenge';
 
 function App() {
-  const [text, setText] = useState('');
-  const [email, setEmail] = useState('');
   return (
-    <section>
-      <Component />
-    </section>
+    <main>
+      <h2>React & Typescript</h2>
+      {/*  <Component type='basic' name='susan' /> */}
+      {/* <Component type='advanced' name='anna' email='email@gmail.com' /> */}
+      <Component type='basic' name='Anna' />
+      <Component type='advanced' name='Susan' email='susan@gmail.com' />
+    </main>
   );
 }
 
