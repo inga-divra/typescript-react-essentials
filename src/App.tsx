2fe8d1b51b8b37e022fab34504909cb69068d002
inga-divra <ingady@gmail.com>
2024-09-18 09:41:33 +0300

    Implemented ThemeProvider using Context API with TypeScript, added dynamic theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-//import Component from './final/05-challenge';
-
-import Component from './starter/05-challenge';
+import Component from './starter/06-context';
 
 function App() {
   return (
     <main>
       <h2>React & Typescript</h2>
-      {/*  <Component type='basic' name='susan' /> */}
-      {/* <Component type='advanced' name='anna' email='email@gmail.com' /> */}
-      <Component type='basic' name='Anna' />
-      <Component type='advanced' name='Susan' email='susan@gmail.com' />
+      <Component />
     </main>
   );
 }
